2a4833742d72765039f2d60d669c5d0d60622cde
Knox-Radox <74523170+Knox-Radox@users.noreply.github.com>
2020-11-16 10:28:44 +0530

    Add files via upload

---
+++ chatroom.py
@@ -0,0 +1,48 @@
+import threading
+import socket
+
+host = ''
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+	for client in clients:
+		client.send(message)
+
+def handle(client):
+	while True:
+		try:
+			message = client.recv(1024)
+			broadcast(message)
+		except:
+			index = clients.index(client)
+			clients.remove(client)
+			client.close()
+			nickname = nicknames[index]
+			broadcast(f'{nickname} has left the chat'.encode('ascii'))
+			nicknames.remove(nickname)
+			break
+
+def receive():
+	while True:
+		client, address = server.accept()
+		print(f"Connected With {str(address)}")
+
+		client.send("NICK".encode('ascii'))
+		nickname = client.recv(1024).decode('ascii')
+		clients.append(client)
+
+		print(f"Nickname of The Client is {nickname}")
+		broadcast(f'{nickname} joined the chat'.encode('ascii'))
+		client.send("Connected to the server".encode('ascii'))
+
+		thread = threading.Thread(target=handle, args=(client,))
+		thread.start()
+print("Server Has Started")
+receive()
